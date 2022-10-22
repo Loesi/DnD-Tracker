86e8b43b1252178249ad0ffa5b0c05650251c02a
Loesi <jannik.loeseke@gmail.com>
2022-10-22 19:15:28 +0200

    see prev

--- website/models.py
+++ website/models.py
@@ -4,16 +4,16 @@ class Encounter(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(16))
     iniGiven =db.Column(db.Boolean())
-    entities = db.C
+    #entities = db.C
 
 
 class Entity(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     maxHp = db.Column(db.Integer)
     hp = db.Column(db.Integer)
-    str = db.Column(db.Tuple)
-    dex = db.Column(db.Tuple)
-    con = db.Column(db.Tuple)
-    wis = db.Column(db.Tuple)
-    int = db.Column(db.Tuple)
-    cha = db.Column(db.Tuple)
+    #str = db.Column(db.Tuple)
+    #dex = db.Column(db.Tuple)
+    #con = db.Column(db.Tuple)
+    #wis = db.Column(db.Tuple)
+    #int = db.Column(db.Tuple)
+    #cha = db.Column(db.Tuple)
